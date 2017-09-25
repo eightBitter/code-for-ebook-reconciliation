4bb487a12f42da761ac07d53d002f10ab7d0592d
Jacob Shelby <jtshelby@ncsu.edu>
2017-09-25 17:26:33 -0400

    Added code to retrieve redirected URLs using cleaned URLs; added grel code for cleaning URLs using OpenRefine

--- urlRedirect.py
+++ urlRedirect.py
@@ -6,3 +6,25 @@ import requests
 #Retrieve a single URL redirect
 r = requests.get('https://link.springer.com/openurl?genre=book&isbn=978-1-4939-3677-9')
 print(r.url)
+
+### open/read text file, supply delimiter, print each row
+import csv
+with open('Sirsi_eBooks.txt', 'rb') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
+    for row in reader:
+        print row
+
+### open/read text file, supply delimiter, print each row of the column URL
+import csv
+with open('Sirsi_eBooks.txt', 'rb') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
+    for row in reader:
+        print(row['URL'])
+
+### open/read text file, grab the URL from URL column. Redirect each URL and retrieve the redirected URL
+import csv
+import requests
+with open('springer-ebook-url-cleaned.csv', 'rb') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(requests.get(row['URL']).url)
