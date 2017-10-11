dafc158d16fe2cc09498c9309e93080f2f914110
Jacob Shelby <jtshelby@ncsu.edu>
2017-10-11 10:21:22 -0400

    Updated workflow doc, added script to split csv into batches

--- singleRedirectErrorTest.py
+++ singleRedirectErrorTest.py
@@ -5,9 +5,9 @@ import csv
 import requests
 
 #Open file with cleaned URLs. Also open a new file where you'll write the redirected URLs
-with open('springer-ebook-url-cleaned.csv', 'rb') as originalFile, open('springer-ebook-url-redirected.csv', 'wb') as redirectedFile:
+with open('springer-ebook-url-cleaned-20170929.csv', 'rb') as originalFile, open('springer-ebook-url-redirected-2.csv', 'wb') as redirectedFile:
     #Set column names for springer-ebook-url-redirected.csv
-    fieldnames = ['title', 'URL']
+    fieldnames = ['title', 'URL', 'errorMessage']
     #Set variables for the two datasets
     reader = csv.DictReader(originalFile)
     writer = csv.DictWriter(redirectedFile, fieldnames=fieldnames)
@@ -16,14 +16,25 @@ with open('springer-ebook-url-cleaned.csv', 'rb') as originalFile, open('springe
     writer.writeheader()
     #Read each row in the first file; add the title to the new file; redirect each URL and add the redirected URL to the new file
     for row in reader:
-        redirectedURL = requests.get(row['URL']).url
-        # for line in writer:
-        writer.writerow({'title': row['title'], 'URL': redirectedURL})
-            #print(requests.get(row['URL']).url)
+        # Try to redirect the URLs
+        try:
+            redirectedURL = requests.get(row['URL']).url
+            # for line in writer:
+            writer.writerow({'title': row['title'], 'URL': redirectedURL})
+                #print(requests.get(row['URL']).url)
+        # If redirecting the URL results in an erro (e.g. the URL times out), then add the error message to the csv and keep running through the rest of the URLs (keeps the script from stopping based on the error)
+        except Exception, message:
+            writer.writerow({'title': row['title']})
+            writer.writerow({'errorMessage': str(message)})
+
 print "ALL DONE!"
 
 ### TODO
-# Figure out how to bypass connection issues for single URL problems
-# requests.exceptions.ConnectionError: HTTPSConnectionPool(host='link.springer.com', port=443): Max retries exceeded with url: /openurl?genre=book&isbn=978-3-319-46184-7 (Caused by NewConnectionError('<urllib3.connection.VerifiedHTTPSConnection object at 0x105dc3f10>: Failed to establish a new connection: [Errno 60] Operation timed out',))
-# Insert an error message in the new file to indicate there was a problem with the particular URL
-# Using the code block from Trello and test it against the open URL
+#  Field limit problem:
+#  File "singleRedirectErrorTest.py", line 18, in <module>
+#     for row in reader:
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/csv.py", line 108, in next
+#     row = self.reader.next()
+# _csv.Error: field larger than field limit (131072)
+# Problem was created, because OpenRefine messed up some of the column values
+# Need to start with a file that doesn't have messed up column values
--- workflow.md
+++ workflow.md
@@ -3,5 +3,16 @@ URL Redirect Workflow
 
 - Open the Sirsi list in OpenRefine
 - Run the cleanUpURL.grel code to clean up the URL column
+- If the number of records is large (it takes 24 hours to process 10,000 URLs
+	- Export as CSV
+	- Break the CSV into batches using splitCSV/csv_splitter.py
+	- Import a batch back into OpenRefine
+- Exclude (facet) any titles that don't need redirects. This reduces the amount of time it'll take to process
+- Click the arrow next to the URL header, choose `Edit column` > `Add column based on this column...`
+- Choose `Python/Jython` from the `Language` dropdown
+- Copy and paste the text in jythonURLRedirect.py into the text box, and press OK
+- Remove current facet, and facet the new column by clicking the arrow > `Facet` > `Customized facets` > `Facet by blank`, and choose `True` on the left
+- Click the arrow again, choose `Edit cells` > `Transform...`
+- Write `cells["URL"].value` in the text box to fill in any blank cells with URLs from the URL column
+- Delete the `URL` column, rename new column `URL`
 - Export as CSV
-- Run urlRedirect.py to get redirect URLs; creates springer-ebook-url-redirected.csv
